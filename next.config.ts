51ac6ccc6e0be3d1dea3723494dc9f6ebffd14d7
King Ahmad <kingahmadilyas03@gmail.com>
2026-08-09 07:55:41 +0800

    chore(deploy): add docker-compose, dockerfile, and nginx config for production

--- next.config.ts
+++ next.config.ts
@@ -37,7 +37,7 @@ const nextConfig: NextConfig = {
       },
     ];
   },
-  // output: 'standalone',
+  output: 'standalone', // local
   webpack: (config, { nextRuntime }) => {
     if (nextRuntime === "edge") {
       config.resolve.alias = {
